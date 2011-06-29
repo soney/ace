f5da0e6c807fc0fa53502be39bec0bec26955599
Stephen Oney <swloney@gmail.com>
2011-06-29 15:41:42 -0700

    Moved some livedoc functionality out of ace

--- build/src/ace-uncompressed.js
+++ build/src/ace-uncompressed.js
@@ -6656,8 +6656,10 @@ var Editor =function(renderer, session) {
         this.renderer.destroy();
     };
 
-	this.invokeLivedoc = function() {
-		console.log("LIVEDOC!");
+	this.setLivedoc = function(livedoc) {
+		this.livedoc = livedoc;
+		this.livedoc.setEditor(this);
+		console.log("OK");
 	};
 
 }).call(Editor.prototype);
@@ -7257,7 +7259,7 @@ exports.MouseHandler = MouseHandler;
  *
  * ***** END LICENSE BLOCK ***** */
 
-define('ace/keyboard/keybinding', ['require', 'exports', 'module' , 'pilot/useragent', 'pilot/keys', 'pilot/event', 'pilot/settings', 'pilot/canon', 'ace/commands/default_commands', 'ace/commands/livedoc_commands'], function(require, exports, module) {
+define('ace/keyboard/keybinding', ['require', 'exports', 'module' , 'pilot/useragent', 'pilot/keys', 'pilot/event', 'pilot/settings', 'pilot/canon', 'ace/commands/default_commands'], function(require, exports, module) {
 
 var useragent = require("pilot/useragent");
 var keyUtil  = require("pilot/keys");
@@ -7265,7 +7267,6 @@ var event = require("pilot/event");
 var settings  = require("pilot/settings").settings;
 var canon = require("pilot/canon");
 require("ace/commands/default_commands");
-require("ace/commands/livedoc_commands");
 
 var KeyBinding = function(editor) {
     this.$editor = editor;
@@ -7700,66 +7701,6 @@ canon.addCommand({
 });
 
 });/* vim:ts=4:sts=4:sw=4:
- * ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Ajax.org Code Editor (ACE).
- *
- * The Initial Developer of the Original Code is
- * Ajax.org B.V.
- * Portions created by the Initial Developer are Copyright (C) 2010
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *      Fabian Jakobs <fabian AT ajax DOT org>
- *      Julian Viereck <julian.viereck@gmail.com>
- *      Mihai Sucan <mihai.sucan@gmail.com>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
-
-define('ace/commands/livedoc_commands', ['require', 'exports', 'module' , 'pilot/lang', 'pilot/canon'], function(require, exports, module) {
-
-var lang = require("pilot/lang");
-var canon = require("pilot/canon");
-
-function bindKey(win, mac) {
-    return {
-        win: win,
-        mac: mac,
-        sender: "editor"
-    };
-}
-
-canon.addCommand({
-    name: "invokeLivedoc",
-    bindKey: bindKey("Ctrl-/", "Command-/"),
-    exec: function(env, args, request) { env.editor.invokeLivedoc(); }
-});
-
-});
-/* vim:ts=4:sts=4:sw=4:
  * ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
